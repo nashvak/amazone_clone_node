6b2d6ed3f1da6099bf56569d31b3a5b8de1c64c2
nashva <nashvakizhakkayil@gmail.com>
2024-01-25 12:03:29 +0530

    colors added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,9 +13,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Ecommerce',
       theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
         useMaterial3: true,
       ),
-      home: Scaffold(body: Text('fsd')),
+      home: const Scaffold(body: Text('fsd')),
     );
   }
 }
---
+++ lib/router.dart
@@ -0,0 +1,8 @@
+import 'package:ecommerce_app/features/auth/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
+    case AuthScreen:
+  }
+}
