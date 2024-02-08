21a3b24430ef0d076f23aaf741ae8218d6d8026c
nashva <nashvakizhakkayil@gmail.com>
2024-02-08 23:15:18 +0530

    appbar for profile screen

--- lib/features/screens/profile/profile_screen.dart
+++ lib/features/screens/profile/profile_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../constants/global_variables.dart';
+import '../../providers/auth/user_provider.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,6 +14,76 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final user = Provider.of<UserProvider>(context).user;
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: GlobalVariables.appBarGradient,
+            ),
+          ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                child: Image.asset(
+                  'images/amazon_in.png',
+                  width: 120,
+                  height: 45,
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                // color: Colors.red,
+                // padding: const EdgeInsets.only(left: 15, right: 15),
+                child: const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      child: Icon(Icons.notifications_outlined),
+                    ),
+                    Icon(
+                      Icons.search,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                gradient: GlobalVariables.appBarGradient,
+              ),
+              padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              child: RichText(
+                text: TextSpan(
+                  text: 'Hello, ',
+                  style: const TextStyle(
+                    fontSize: 22,
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: user.name,
+                      style: const TextStyle(
+                        fontSize: 22,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              ))
+        ],
+      ),
+    );
   }
 }
