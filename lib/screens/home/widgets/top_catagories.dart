24aaaf4b5c4d1c5f3d629581269d311eefa4b651
nashva <nashvakizhakkayil@gmail.com>
2024-02-10 23:49:59 +0530

    static images added

---
+++ lib/screens/home/widgets/top_catagories.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TopCategories extends StatelessWidget {
+  const TopCategories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      height: 60,
+      child: ListView.builder(itemBuilder: (context,index){
+        return 
+      }),
+    );
+  }
+}
