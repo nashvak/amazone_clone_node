166f275477fd159e1a1bdc3ffb8ca779e2eb55a7
nashva <nashvakizhakkayil@gmail.com>
2024-02-11 22:53:09 +0530

    validation set for navigate to login or homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:ecommerce_app/providers/auth/user_provider.dart';
 // import 'package:ecommerce_app/screens/auth/auth_screen.dart';
 import 'package:ecommerce_app/common/bottonbar.dart';
 import 'package:ecommerce_app/router/router.dart';
+import 'package:ecommerce_app/screens/auth/auth_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -20,20 +21,24 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => UserProvider()),
       ],
       child: MaterialApp(
-          title: 'Ecommerce',
-          theme: ThemeData(
-            scaffoldBackgroundColor: GlobalVariables.backgroundColor,
-            colorScheme: const ColorScheme.light(
-              primary: GlobalVariables.secondaryColor,
-            ),
-            appBarTheme: const AppBarTheme(
-              elevation: 0,
-              iconTheme: IconThemeData(color: Colors.black),
-            ),
-            // useMaterial3: true,
+        title: 'Ecommerce',
+        theme: ThemeData(
+          scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+          colorScheme: const ColorScheme.light(
+            primary: GlobalVariables.secondaryColor,
           ),
-          onGenerateRoute: ((settings) => generateRoute(settings)),
-          home: const BottomBar()),
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            iconTheme: IconThemeData(color: Colors.black),
+          ),
+          // useMaterial3: true,
+        ),
+        onGenerateRoute: ((settings) => generateRoute(settings)),
+        // home: const BottomBar(),
+        home: Provider.of<UserProvider>(context).user.token.isNotEmpty
+            ? const BottomBar()
+            : const AuthScreen(),
+      ),
     );
   }
 }
