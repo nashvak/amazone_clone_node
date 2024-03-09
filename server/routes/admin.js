188342a68a3e9d66bfdc8def689dd022bbd84802
nashva <nashvakizhakkayil@gmail.com>
2024-03-09 21:10:48 +0530

    api created for delete the product

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -24,4 +24,17 @@ res.json(products); // return the products
 res.status(500).json({error:e.message});
     }
 });
+
+//deleting the product
+adminRouter.get("/admin/delete-product",admin,async (req,res)=>{
+try {
+    const {id}=req.body;
+    let product=await Product.findByIdAndDelete(id);
+    product=await product.save();
+    res.json(product);
+} catch (e) {
+    res.status(500).json({error:e.message});
+    
+}
+})
 module.exports=adminRouter;
