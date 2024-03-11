623be399a7961ce170e8a418fd3b508b5013d0c5
nashva <nashvakizhakkayil@gmail.com>
2024-03-11 14:02:39 +0530

    creating api for retrieving the searched product

--- lib/user_panel/features/search/screens/search_screen.dart
+++ lib/user_panel/features/search/screens/search_screen.dart
@@ -1,4 +1,8 @@
+import 'package:ecommerce_app/admin_panel/screens/widgets/loader.dart';
+import 'package:ecommerce_app/user_panel/features/search/services/search_services.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../admin_panel/model/product_model.dart';
 
 class SearchScreen extends StatefulWidget {
   static const String routeName = '/search-screen';
@@ -10,10 +14,27 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final SearchServices searchServices = SearchServices();
+  List<Product>? products;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchSearchedProduct();
+  }
+
+  fetchSearchedProduct() async {
+    products = await searchServices.fetchSearchedProduct(
+        context: context, searchQuery: widget.searchQuery);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text(widget.searchQuery)),
-    );
+    return products == null
+        ? const Loader()
+        : Scaffold(
+            body: Center(child: Text(widget.searchQuery)),
+          );
   }
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -6,7 +6,7 @@ const Product=require("../user_model/product");
 // "/api/products?category=Essentials"
 productRouter.get('/api/products',auth,async(req,res)=>{
     try{
-        console.log(req.query.category);
+        // console.log(req.query.category); when we use ? in url as parameter
 const products=await Product.find({category:req.query.category});
 res.json(products); // return the products 
 
@@ -14,4 +14,21 @@ res.json(products); // return the products
 res.status(500).json({error:e.message});
     }
 });
+
+//create a get request to search products and get them
+
+productRouter.get('/api/products/search/:name',auth,async(req,res)=>{
+    try{
+        // console.log(req.params.category); when we use : in url as parameter
+const products=await Product.find({
+    name: { $regex: req.params.name, $options: "i" }, //when we type i for iphone it shown
+    // console.log(req.params.category); - 
+});
+res.json(products); // return the products 
+
+    }catch(e){
+res.status(500).json({error:e.message});
+    }
+});
+
 module.exports=productRouter;
